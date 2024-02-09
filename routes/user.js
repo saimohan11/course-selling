76f551f1bfd3dd8d1efbd6bd33247d4ef388a743
saimohan <saimohan11sai@gmail.com>
2024-02-09 15:56:04 +0530

    added course routes in user

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,41 @@ router.get("/courses",async (req,res)=>{
     const courses = await Course.find({})
     res.json({
         courses
+    });
+});
+
+router.post("/couses/:courseId",userMiddleware,async (req,res)=>{
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
     })
-})
+    res.json({
+        message: "Purchase complete!"
+    })
+});
+
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
+    // Implement fetching purchased courses logic
+    const user = await User.findOne({
+        username: req.headers.username
+    });
+
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    });
+
+    res.json({
+        courses: courses
+    })
+});
 
 module.exports = router;
